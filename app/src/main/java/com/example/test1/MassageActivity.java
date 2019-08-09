ecb6bd6a61e526068f8879fe3c0e9c164e7e8399
Pubudu Arosha <it18108514@my.slii.lk>
2019-08-09 12:09:50 +0530

    Submit Message and It Display Message Activity

--- app/src/main/java/com/example/test1/MassageActivity.java
+++ app/src/main/java/com/example/test1/MassageActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MassageActivity extends AppCompatActivity {
+public class MassageActivity<messgae> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_massage);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra("ExTRA_MESSAGE");
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
+
     }
+
 
 
 }
